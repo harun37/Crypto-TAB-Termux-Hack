e8464c8ee7075e80872956ff8b7ca8649dd90f31
harun37 <64012121+harun37@users.noreply.github.com>
2020-04-20 14:03:01 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Optional*/
+/*Boleh Diganti, Boleh Tidak*/
+
+$boost_url = "av=1.0.0.84&v=4.0.8&bid=0c40fea0-648c-44dd-8d8d-ca5ee5f639a6&app_id=pro.cryptotab.android";
+$start = "bid=592a0cf1-d9dc-41a1-92ab-ccb81c8e25a5&v=4.0.8";
+$versi = "av=1.0.0.84&v=4.0.8&bid=0c40fea0-648c-44dd-8d8d-ca5ee5f639a6";
+$data_boost = '{"boost_ratio":2}';
+
+/*Change According to Your Auth Data*/
+/*Ubah Sesuai Data Auth Kalian*/
+
+$auth[] = "isi data auth kalian";
+$auth[] = "isi data auth kalian";
